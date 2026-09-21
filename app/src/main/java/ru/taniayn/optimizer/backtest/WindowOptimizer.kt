f0410384b124275917b682129c68785177970c88
Tat-T <ring_87_87@mail.ru>
2026-09-21 20:57:20 +0300

    список окон для автоматической проверки

---
+++ app/src/main/java/ru/taniayn/optimizer/backtest/WindowOptimizer.kt
@@ -0,0 +1,21 @@
+package ru.taniayn.optimizer.backtest
+
+object WindowOptimizer {
+
+    /**
+     * Размеры исторического окна,
+     * которые будем автоматически проверять.
+     */
+    val windows = listOf(
+        20,
+        30,
+        40,
+        50,
+        60,
+        80,
+        100,
+        120,
+        150,
+        200
+    )
+}
